94e22e12ae36647d952fab591317c3b3a9ea72e6
Scott Edwards <scotteza@gmail.com>
2019-05-23 15:03:53 +0100

    Add script to create users table

---
+++ 001 Basics/001 schema.sql
@@ -0,0 +1,19 @@
+﻿drop table if exists users;
+
+create or replace function the_time() returns timestamptz as
+$$
+	select now() as result;
+$$ language sql;
+
+create table users (
+	id serial primary key not null,
+	email varchar(255) unique not null,
+	first varchar(50),
+	last varchar(50),
+	created_at timestamptz not null default the_time()
+);
+
+insert into users(email, first, last)
+values ('test@test.com', 'Scott', 'Edwards');
+
+select * from users;

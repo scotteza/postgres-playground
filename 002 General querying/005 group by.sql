3c5ecfde245c4e89a9ceb8b90e49a9051cc71dde
Scott Edwards <scotteza@gmail.com>
2019-05-24 11:24:04 +0100

    Learn about using windowing functions to do multiplerollups

--- 002 General querying/005 group by.sql
+++ 002 General querying/005 group by.sql
@@ -1,3 +1,24 @@
-﻿with actor_rollup as (select first_name, count(first_name) as name_count from actor group by first_name)
+﻿/*
+with actor_rollup as (select first_name, count(first_name) as name_count from actor group by first_name)
 
 select * from actor_rollup order by name_count desc
+*/
+
+with actor_rollup as
+	(select
+		a.actor_id,
+		a.first_name,
+		a.last_name,
+		count(1) over(partition by a.first_name) as first_name_count,
+		count(1) over(partition by a.last_name) as last_name_count,
+		count(1) over(partition by a.first_name, last_name) as full_name_count
+	from
+		actor a)
+select distinct
+	ar.*
+from
+	actor_rollup ar
+order by
+	ar.full_name_count desc,
+	ar.first_name,
+	ar.last_name

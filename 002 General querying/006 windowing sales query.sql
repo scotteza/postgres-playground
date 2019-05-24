4b1c2cbd557dd1cc1c097ba3998fc7774bc9801b
Scott Edwards <scotteza@gmail.com>
2019-05-24 11:32:10 +0100

    Add raw_sales view

---
+++ 002 General querying/006 windowing sales query.sql
@@ -0,0 +1,21 @@
+﻿create or replace view raw_sales as
+select
+	f.title,
+	f.description,
+	f.length,
+	f.rating,
+	p.amount,
+	p.payment_date,
+	date_part('quarter', p.payment_date) as quarter,
+	date_part('month', p.payment_date) as month,
+	date_part('year', p.payment_date) as year,
+	concat('Q', date_part('quarter', p.payment_date)::text, '-', date_part('year', p.payment_date)::text) as qyear,
+	cash_words(amount::money) as spelling_it_out,
+	to_tsvector(concat(title, description)) as search_field
+from
+	film f
+	inner join inventory i on i.film_id = f.film_id
+	inner join rental r on r.inventory_id = i.inventory_id
+	inner join payment p on p.rental_id = r.rental_id;
+
+select * from raw_sales;

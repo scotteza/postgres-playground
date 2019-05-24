689d7b2d4ad3d416125d59061535627b0c0bed84
Scott Edwards <scotteza@gmail.com>
2019-05-24 16:46:45 +0100

    Update raw_sales view to return correct data types

--- 002 General querying/008 update raw sales view.sql
+++ 002 General querying/008 update raw sales view.sql
@@ -1,3 +1,5 @@
+drop view if exists raw_sales;
+
 create or replace view raw_sales as
 select
 	f.title,
@@ -6,12 +8,12 @@ select
 	f.rating,
 	p.amount,
 	p.payment_date,
-	date_part('quarter', p.payment_date) as quarter,
-	date_part('month', p.payment_date) as month,
-	date_part('year', p.payment_date) as year,
+	date_part('quarter', p.payment_date)::integer as quarter,
+	date_part('month', p.payment_date)::integer as month,
+	date_part('year', p.payment_date)::integer as year,
 	concat('Q', date_part('quarter', p.payment_date)::text, '-', date_part('year', p.payment_date)::text) as qyear,
 	cash_words(amount::money) as spelling_it_out,
-	to_tsvector(concat(title, description)) as search_field
+	to_tsvector(concat(title, ' ', description)) as search_field
 from
 	film f
 	inner join inventory i on i.film_id = f.film_id

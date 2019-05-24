931c8e4bbcf22f19c65dda3d6e6e2476de244758
Scott Edwards <scotteza@gmail.com>
2019-05-24 10:56:19 +0100

    Add function to return random records from a table

---
+++ 002 General querying/004 using ranges on table data.sql
@@ -0,0 +1,19 @@
+﻿create or replace function random_payments(counter int) returns setof payment
+as
+$$
+	select
+		*
+	from
+		payment p
+	where
+		p.payment_id in
+			(select
+				trunc((random() * (32098 - 17503)) + 17503)
+			from
+				generate_series(1, counter));
+
+	-- select min(payment_id), max(payment_id) from payment;
+$$
+language sql;
+
+select * from random_payments(42);

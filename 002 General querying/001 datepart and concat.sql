74cdcf618ddb8a68d4335e6f9ff6094bd61b8e4d
Scott Edwards <scotteza@gmail.com>
2019-05-24 10:34:29 +0100

    Play with datepart and concat

---
+++ 002 General querying/001 datepart and concat.sql
@@ -0,0 +1,10 @@
+﻿select
+	amount,
+	payment_date,
+	date_part('quarter', payment_date) as quarter,
+	date_part('year', payment_date) as year,
+	concat('Q', date_part('quarter', payment_date), '-', date_part('year', payment_date)) as quarter_description
+from
+	payment p
+order by
+	payment_date
